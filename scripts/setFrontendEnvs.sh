078365e950e1073a58536921cd57a89dddc3d949
manv6 <evlastos@gmail.com>
2024-04-30 16:05:41 +0300

    create .env file wth sh script

--- scripts/setFrontendEnvs.sh
+++ scripts/setFrontendEnvs.sh
@@ -5,11 +5,9 @@ echo REACT_APP_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID >> testerloop-frontend/.env
 echo REACT_APP_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> testerloop-frontend/.env
 echo REACT_APP_AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN >> testerloop-frontend/.env
 
-cat  testerloop-frontend/.env
-
 echo REACT_APP_AWS_REGION=$AWS_REGION > .env
 echo REACT_APP_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID >> .env
 echo REACT_APP_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> .env
 echo REACT_APP_AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN >> .env
 
-cat  .env
+echo 'testerloop-frontend/.env and .env created' 
